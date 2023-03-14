7acdff0fd3f32bbcf4affc88f2845a4ddccbba0d
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-03-14 15:11:39 +0100

    Generalised code for more upgrades and added upgrade3

--- script.js
+++ script.js
@@ -4,29 +4,40 @@
 let cookers = 0;
 let cps = 0;
 let refreshRate = 50;
-let upgradeLevelList = [0, 0];
-let baseList = [0, 5];
-let rateGrowthList = [0, 1.07];
-let cpsIncreaseList = [0, 1]; //Actual increase is divided by 10
+let upgradeLevelList = [0, 0, 0];
+let baseList = [40, 5, 60];
+let rateGrowthList = [2.5, 1.07, 1.15];
+let cpsIncreaseList = [0, 2, 120]; //Actual increase is divided by 10
+let upgradeNameList = ["Cookers per Click", "Dough slappers", "Batter blaster"];
 setInterval(numberUpdater, refreshRate);
 document.querySelector("#cooker_sprite").addEventListener("click", cookerClicker);
-document.querySelector("#upgrade1 button").addEventListener("click", upgradeClicker);
-updateWhenClicked();
+
+upgradeInit(0);
+upgradeInit(1);
+upgradeInit(2);
+function upgradeInit(upgradeNum) {
+  document.querySelector("#upgrade" + upgradeNum + " button").addEventListener("click", upgradeClicker);
+  updateWhenClicked(upgradeNum);
+}
 
 function numberUpdater() {
   // console.log(cps);
   cookers = cookers + cps * (refreshRate / 10000);
   document.querySelector("#total_cookers").textContent = Math.floor(cookers) + " cookers";
 }
-function updateWhenClicked() {
+function updateWhenClicked(upgradeNum) {
   // console.log("updateWhenClicked");
   document.querySelector("#cookers_per_sec").textContent = `${cps / 10} cps`;
-  document.querySelector("#upgrade1_cost").textContent = `price: ${costFinder(5, 1.07, upgradeLevelList[1])}`;
-  document.querySelector("#upgrade1_amount").textContent = `Dough slappers: ${upgradeLevelList[1]}`;
+  document.querySelector("#upgrade" + upgradeNum + "_cost").textContent = `price: ${costFinder(baseList[upgradeNum], rateGrowthList[upgradeNum], upgradeLevelList[upgradeNum])}`;
+  if (upgradeNum == 0) {
+    document.querySelector("#upgrade0_amount").textContent = `${upgradeNameList[upgradeNum]}: ${2 ** upgradeLevelList[upgradeNum]}`;
+  } else {
+    document.querySelector("#upgrade" + upgradeNum + "_amount").textContent = `${upgradeNameList[upgradeNum]}: ${upgradeLevelList[upgradeNum]}`;
+  }
 }
 function cookerClicker() {
   let cooker = this;
-  cookers++;
+  cookers += 2 ** upgradeLevelList[0];
   cooker.classList.remove("clicked");
   cooker.offsetLeft;
   cooker.classList.add("clicked");
@@ -41,19 +52,12 @@ function upgradeClicker() {
     cookers = cookers - cost;
     upgradeLevelList[upgradeNum]++;
 
-    cps += cpsIncreaseList(upgradeNum);
+    cps += cpsIncreaseList[upgradeNum];
   }
-  updateWhenClicked();
+  updateWhenClicked(upgradeNum);
 }
 function costFinder(base, rateGrowth, owned) {
   let cost = (base * rateGrowth ** owned).toFixed(2);
   console.log(`base: ${base}, rateGrowth: ${rateGrowth}, owned: ${owned}, cost: ${cost}`);
   return cost;
-}
-
-function cpsIncreaseList(num) {
-  switch (num) {
-    case "1":
-      return 1;
-  }
 }
